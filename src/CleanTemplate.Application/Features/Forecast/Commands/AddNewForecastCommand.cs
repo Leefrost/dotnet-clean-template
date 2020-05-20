ffa642b150cb8f83ee483904a43e7cb49270242d
Leefrost <evertodante@gmail.com>
2020-05-20 11:53:00 +0300

    dev: Draft for validation pipeline and queue handling

--- src/CleanTemplate.Application/Features/Forecast/Commands/AddNewForecastCommand.cs
+++ src/CleanTemplate.Application/Features/Forecast/Commands/AddNewForecastCommand.cs
@@ -2,31 +2,33 @@
 using System.Threading.Tasks;
 using CleanTemplate.Application.Common;
 using CleanTemplate.Domain.Entities.Forecasts;
-using FluentValidation;
 using MediatR;
 
-namespace CleanTemplate.Application.Features.WeatherForecast.Commands
+namespace CleanTemplate.Application.Features.Forecast.Commands
 {
     public class AddNewForecastCommand : BaseCqrsRequest<WeatherForecastCreation>
     {
-        public AddNewForecastCommand(string appUser)
+        public AddNewForecastCommand(string appUser, WeatherForecastCreation creationData)
             : base(appUser)
         {
+            CreationData = creationData;
         }
+
+        public WeatherForecastCreation CreationData { get; }
+
     }
 
     internal class AddNewForecastCommandHandler
         : IRequestHandler<AddNewForecastCommand, WeatherForecastCreation>
     {
-        private readonly IValidator<WeatherForecastCreation> _validator;
 
-        public AddNewForecastCommandHandler(IValidator<WeatherForecastCreation> validator)
+        public AddNewForecastCommandHandler()
         {
-            _validator = validator;
         }
 
         public Task<WeatherForecastCreation> Handle(AddNewForecastCommand request, CancellationToken cancellationToken)
         {
+
             return Task.FromResult<WeatherForecastCreation>(null);
         }
     }
---
+++ src/CleanTemplate.Application/Features/Forecast/Queries/GetAllForecastForLocationQuery.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CleanTemplate.Application.Common;
+using CleanTemplate.Domain.Entities.Forecasts;
+using MediatR;
+
+namespace CleanTemplate.Application.Features.Forecast.Queries
+{
+    public class GetAllForecastForLocationQuery : BaseCqrsRequest<IQueryable<WeatherForecast>>
+    {
+        public GetAllForecastForLocationQuery(string appUser, WeatherForecastCreation creationData)
+            : base(appUser)
+        {
+            CreationData = creationData;
+        }
+
+        public WeatherForecastCreation CreationData { get; }
+
+    }
+
+    internal class GetAllForecastForLocationQueryHandler
+        : IRequestHandler<GetAllForecastForLocationQuery, IQueryable<WeatherForecast>>
+    {
+
+        public GetAllForecastForLocationQueryHandler()
+        {
+        }
+
+        public Task<IQueryable<WeatherForecast>> Handle(GetAllForecastForLocationQuery request, CancellationToken cancellationToken)
+        {
+
+            return Task.FromResult<IQueryable<WeatherForecast>>(null);
+        }
+    }
+}
